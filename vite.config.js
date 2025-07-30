3475bae5312df90bf73c1980d518922d32dfd6fb
Romuald Brillout <git@brillout.com>
2025-07-30 10:16:37 +0200

    reproduce

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,28 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), {
+    name: 'logger', configResolved(config) {
+      console.log(`[${config.build.ssr ? 'server' : 'client'}] config.build.cssTarget`, config.build.cssTarget)
+    }
+  }],
+  environments: {
+    ssr: {
+      build: {
+        target: 'es2022',
+      }
+    }
+  },
+  build: {
+    ssrEmitAssets: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.endsWith('.css')) {
+            return id.split('/').pop().split('.')[0]
+          }
+        }
+      }
+    }
+  }
 })
